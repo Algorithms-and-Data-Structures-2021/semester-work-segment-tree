f1569f7c31280b0233b519cbbff0030232d85fed
Алексей Михайлов <6978420-Defort@users.noreply.gitlab.com>
2021-04-11 14:36:08 +0300

    add small configs

--- include/segment_tree.hpp
+++ include/segment_tree.hpp
@@ -10,18 +10,29 @@ namespace itis {
   inline constexpr auto kStringConstant = "Hello, stranger!";
 
   // Пример: объявление структуры с полями и методами
-  struct MyStructure {
-   public:
-    int size_{0};
-    int capacity_{0};
-    int* data_{nullptr};
+  struct SegmentTree {
+   private:
+    int size_ {0};
+    int *array_ {nullptr};
+    int *tree {nullptr};
 
     // Tip 2: На начальном этапе разработки структуры данных можете определения методов задавать в
     // заголовочном файле, как только работа будет завершена, можно будет оставить здесь только объявления.
+   public:
 
-    int size() const {
-      return size_;
-    }
+    SegmentTree(int size, int *array);
+
+    ~SegmentTree();
+
+    int get_min(int l, int r);
+
+    int get_max(int l, int r);
+
+    int get_sum(int l, int r);
+
+    void update(int index, int value);
+
+    [[maybe_unused]] int size() const;
   };
 
 }  // namespace itis
